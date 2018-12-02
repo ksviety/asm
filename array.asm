4de5be5d414c7ea481c19b7b34b0914b4edc26c9
ksviety <ksviety@gmail.com>
2018-12-02 10:30:09 +0500

    Compile script

--- array.asm
+++ array.asm
@@ -2,6 +2,9 @@ section .data
 	CharArray: db "H", "I"
 	temp: db 0
 
+	msg: db "Element: ", 0
+	msg_len equ $-msg
+
 section .text
 	global _start
 
@@ -10,13 +13,21 @@ _start:
 	mov eax, [CharArray+1]
 	mov [temp], eax
 
-	mov eax, 4
-	mov ebx, 1
+	mov ecx, msg
+	mov edx, msg_len
+	call _print
+
 	mov ecx, temp
-	mov edx, 2
-	int 0x80
+	mov edx, 1
+	call _print
 
 	call _exit
+
+_print:
+	mov eax, 4
+	mov ebx, 1
+	int 0x80
+	ret
 
 _exit:
 	mov eax, 1
